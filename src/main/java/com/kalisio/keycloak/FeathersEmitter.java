82595f9a921865a5bcfb1bd9edb2f13a29ea9dac
cnouguier <ch.nouguier@gmail.com>
2024-04-03 13:57:14 +0200

    style: unused import commented

--- src/main/java/com/kalisio/keycloak/FeathersEmitter.java
+++ src/main/java/com/kalisio/keycloak/FeathersEmitter.java
@@ -1,11 +1,11 @@
 package com.kalisio.keycloak;
 
-import java.io.BufferedReader;
+//import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+// import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+// import java.net.MalformedURLException;
 import java.net.URL;
 
 public class FeathersEmitter {
